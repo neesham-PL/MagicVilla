b0599bd8774a07191e2485f3e4bdc47e75d53d13
Neesham.Kalia <Neesham.Kalia@BERNABEU>
2023-05-17 11:36:11 +0530

    override

---
+++ MagicVilla_VillaAPI/MappingConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Villa, VillaDTO>();    //.for helps us to use custom Mapping
+            CreateMap<VillaDTO, Villa>();
+
+            CreateMap<Villa, VillaCreateDTO>().ReverseMap();   //using reverse helps use auto create the reverse as as well
+            CreateMap<Villa, VillaUpdateDTO>().ReverseMap();
+
+
+            CreateMap<VillaNumber, VillaNumberDTO>().ReverseMap();
+            CreateMap<VillaNumber, VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumber, VillaNumberUpdateDTO>().ReverseMap();
+            CreateMap<ApplicationUser, UserDTO>().ReverseMap();
+        }
+    }
+}
